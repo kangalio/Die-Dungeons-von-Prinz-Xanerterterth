4eb3d683ff7a107cd1363b9a83acf1caa75e0c88
JoschaR <roesser-joscha@gmx.de>
2024-01-03 11:27:39 +0100

    bewegung gecodet
    navigtion agent zu enemy hinzugefügt
    Bewegung in enemy programmiert
    nicht getestet
    müsste fertig sein
    getracktes objekt muss zum nutzen noch in der Klasse move_towards_player geändert werden akteull "player"

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,6 +4,7 @@ var health : int
 var movement_speed : int
 var attack_damage : int 
 var attack_speed : int 
+@onready var navigation_agent = $navigation_agent
 
 func take_damage (damage_taken):
 	if damage_taken > 0:
@@ -13,24 +14,27 @@ func take_damage (damage_taken):
 		print("take_damage")
 	else: 
 		print("fehler in klasse enemy methode take_damage damage_taken ist positiv oder 0")
+
 func die ():
 	print("die")
 	#loot übergben
 	self.queue_free() 
-func move_towards_player (speed, delta_enemy):
-	# man braucht einen deltawert, da sich der enemy in dieser funktion bewegt
-	var player_position = get_node("player").position
-	var direction = (player_position - position).normalized()
-	var velocity = direction * speed
-	move_and_collide(velocity * delta_enemy)
-	# ich habe hier collide und nicht slide verwendet, denn der enemy soll mit
-	# der bewegung aufhören, wenn er den spieler berührt und die hit animation
-	# abspielen
-	print("move")
+
+func move_towards_player ():
+	navigation_agent.set_target_location("player")
+	print ("move towards player")
+	
 func attak (damage_to_deal):
 	print("attack")
+	
 func heal (healing):
 	if healing > 0:
 		health += healing
 	else:
 		print ("healing ist negativ klasse enemy funktion heal")
+
+func _process(delta):
+	if navigation_agent.is_target_reachable() == true:
+		navigation_agent.set_velocity((navigation_agent.get_next_path_position()-self.position).normalized()) * movement_speed * delta
+		move_and_slide()
+	pass
