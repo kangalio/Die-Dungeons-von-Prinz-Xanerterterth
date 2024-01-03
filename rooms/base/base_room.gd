b80d49b13c006274504eac0f38953cd2e9540292
Vincent Di Bacco <96124432+vincentdibacco@users.noreply.github.com>
2024-01-03 12:13:54 +0100

    update base room script
    added enter and exit method for base room

--- rooms/base/base_room.gd
+++ rooms/base/base_room.gd
@@ -1,11 +1,33 @@
 extends Node2D
 
-
+var exit_door = load("res://rooms/base/exit_door.tscn")
+var room_names = ["res://rooms/normal_rooms/room_1.tscn","res://rooms/normal_rooms/room_1.tscn"]
+const ROOM_TRADER_NAME = "res://rooms/trader/trader_room.tscn"
+const ROOM_LOBBY = "res://rooms/"
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var current_room_layout = 0
 
+func _ready():
+	self.add_child(exit_door.instantiate())
+	self.enter_room("normal")
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func exit_room():
+	self.current_room_layout.free_queued()
+	
+
+func enter_room(room_type):
+	var new_room_cls = 0
+	if room_type == "merchant":
+		new_room_cls = load(ROOM_TRADER_NAME)
+	elif room_type == "normal":
+		new_room_cls = load(room_names.pick_random())
+	else:
+		new_room_cls = load(room_names.pick_random())
+	self.current_room_layout = new_room_cls.instantiate()
+	
