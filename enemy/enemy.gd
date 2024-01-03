b1384d6f36e279a29e8d731ac0891ddeb088ce88
JoschaR <roesser-joscha@gmx.de>
2024-01-03 15:18:12 +0100

    movement range_enemy
    movement für rane enemys
    gegner bewegen sich bis sie spieler angreifen können danach nciht mehr

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -11,32 +11,29 @@ func take_damage (damage_taken):
 		health -= damage_taken
 		if health <= 0:
 			die()
-		print("take_damage")
 	else: 
 		print("fehler in klasse enemy methode take_damage damage_taken ist positiv oder 0")
 
 func die ():
-	print("die")
 	#loot übergben
 	self.queue_free() 
 
-func move_towards_player ():
+func move_towards_player (delta):
 	navigation_agent.set_target_position(PLAYER_CHARACTER.global_position)
-	print ("move towards player")
+	if navigation_agent.is_target_reachable() == true:
+		navigation_agent.set_velocity((navigation_agent.get_next_path_position()-self.position).normalized()) * movement_speed * delta
+		move_and_slide()
 	
 func attak (damage_to_deal):
-	print("attack")
+	PLAYER_CHARACTER.take_damage(damage_to_deal)
 	
 func heal (healing):
 	if healing > 0:
 		health += healing
 	else:
 		print ("healing ist negativ klasse enemy funktion heal")
-func _ready():
-	move_towards_player()
+		
 
 func _process(delta):
-	if navigation_agent.is_target_reachable() == true:
-		navigation_agent.set_velocity((navigation_agent.get_next_path_position()-self.position).normalized()) * movement_speed * delta
-		move_and_slide()
-	pass
+	move_towards_player(delta)
+
