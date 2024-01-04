86588f96da0a6c2a6c7996293aed5573cbe228cd
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2024-01-04 10:39:49 +0100

    Heal

--- player/upgrade/helposchen.gd
+++ player/upgrade/helposchen.gd
@@ -4,6 +4,7 @@ extends "res://player/upgrade/upgrade.gd"
 var is_weapon : bool = false
 
 func _ready():
+	print("jetzt kanst du länger leiden")
 	PLAYER_CHARACTER.running_LP = PLAYER_CHARACTER.base_LP
 
 func _process(delta):
