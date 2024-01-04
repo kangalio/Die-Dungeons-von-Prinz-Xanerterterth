0e293b06035086cfc8516c89758e2d9bb69bb4a1
LarsRoss <92991770+LarsRoss@users.noreply.github.com>
2024-01-04 16:55:04 +0100

    fixed traderoom gd and added floor

--- rooms/trader/trader_room.gd
+++ rooms/trader/trader_room.gd
@@ -6,7 +6,7 @@ func _ready():
 	GameManager.open_exit_door()
 	
 func return_interactable():
-	return ["res://interactables/debuggin_tool_on_floor.tscn", "res://interactables/DMG_boost_on_floor.tscn", "res://interactables/helposchen_on_floor.tscn", "res://interactables/speed_bost_on_floor.tscn", "res://interactables/weapon_01_on_floor.tscn"].pick_random()
+	return ["res://interactables/DMG_boost_on_floor.tscn", "res://interactables/helposchen_on_floor.tscn", "res://interactables/speed_bost_on_floor.tscn",].pick_random()
 
 func spawn_interactable(file_path, position):
 	var interactable = load(file_path).instantiate()
