62a9f49dff78159f1e06de193c0de12ca1f0525c
Vincent Di Bacco <96124432+vincentdibacco@users.noreply.github.com>
2024-01-04 09:55:10 +0100

    update game manager
    allows random spawning of enemies on spawn points

--- metadata/game_manager.gd
+++ metadata/game_manager.gd
@@ -8,7 +8,20 @@ var next_room_is_trader = 0
 const ROOM_TRADER = "res://rooms/trader/trader_room.tscn"
 const ROOM_NORMAL = "res://rooms/normal_rooms/room_1.tscn"
 
-var enemy_melee_cls = load("res://enemy/melee/melee_enemy.tscn")
+var enemy_melee_cls = load("res://enemy/melee/hunter.tscn")
+var enemy_melee_paths = [
+	"res://enemy/melee/hunter.tscn",
+	"res://enemy/melee/minion.tscn",
+	"res://enemy/melee/tank.tscn",
+]
+var enemy_ranged_paths = [
+	#"res://enemy/range/range_enemy.tscn"
+]
+var enemy_boss_paths = [
+	"res://enemy/melee/hunter.tscn"
+]
+var enemy_all_paths = enemy_melee_paths + enemy_ranged_paths
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,13 +30,18 @@ func _ready():
 func _process(delta):
 	pass
 
-func spawn_enemy(enemy_type, position):
+func spawn_enemy(enemy_type, position, room):
 	var enemy = 0
-	if enemy_type == "melee":
-		enemy = enemy_melee_cls.instantiate()
-	else:
-		enemy = enemy_melee_cls.instantiate()
-	self.add_child(enemy)
+	var enemy_location = ""
+	match enemy_type:
+		"melee":
+			enemy_location = enemy_melee_paths.pick_random()
+		"random":
+			enemy_location = enemy_all_paths.pick_random()
+		"boss":
+			enemy_location = enemy_boss_paths.pick_random()
+	enemy = load(enemy_location).instantiate()
+	room.add_child(enemy)
 	enemy.set_global_position(position)
 
 	
@@ -40,9 +58,10 @@ func enter_new_room():
 	self.add_child(new_room)
 	var new_pos = new_room.get_node("EnterPoint").global_position
 	PLAYER_CHARACTER.set_global_position(new_pos)
-	var spawn_points = new_room.get_node("EnemySpawnPoints")
+	var spawn_points = new_room.get_node("EnemySpawnPoint")
 	if spawn_points == null:
 		print("Warn: no spawn points available")
 	else:
-		for point in new_room.get_node("EnemySpawnPoints").get_children():
-			self.spawn_enemy("melee",point.global_position)
+		for point in spawn_points.get_children():
+			print("spawning enemy at ",point.gloabl_position)
+			self.spawn_enemy("random",point.global_position, new_room)
