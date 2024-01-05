95294db6b100e9b09b401c3e2be1448eba6f29f7
JoschaR <roesser-joscha@gmx.de>
2024-01-05 09:08:26 +0100

    bullet funktioniert

--- player/character/bullet.gd
+++ player/character/bullet.gd
@@ -10,7 +10,7 @@ func _ready():
 	direction.normalized()
 
 func _process(delta):
-#	$AnimatedSprite2D.play("default")
+	$AnimatedSprite2D.play("default")
 	time -= delta 
 	if time <= 0:
 		queue_free()
@@ -18,9 +18,7 @@ func _process(delta):
 	move_and_slide()
 
 func _on_area_2d_body_entered(body):
-	print("hfds")
 	if body.is_in_group("player") == false:
-		print("!")
 		if body.is_in_group("enemy"):
 			body.take_damage(damage + bonus_damage)
 			queue_free()
