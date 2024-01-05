5d838d168beb83e720fffe007c750f819e3ae693
Vincent Di Bacco <96124432+vincentdibacco@users.noreply.github.com>
2024-01-05 11:04:59 +0100

    Update game_manager.gd
    scale number of spawned enemies with times trader visited

--- metadata/game_manager.gd
+++ metadata/game_manager.gd
@@ -6,11 +6,14 @@ var character_experience:int = 0
 var next_room_is_trader = 0
 var room_counter:int = 0
 var remaining_enemies:int = 0
+var times_trader_was_visited:int = 0
 
 const DEBUG = 1
 const TO_TRADER = 0
 const INF_HEALTH = 0
 const OPEN_DOORS = 1
+
+const NUM_START_ENEMIES:int = 3
 
 const INTERACTABLE_COIN = "res://interactables/coin.tscn"
 const ROOM_TRADER = "res://rooms/trader/trader_room.tscn"
@@ -62,14 +65,16 @@ func spawn_enemy(enemy_type, position):
 	enemy.set_global_position(position)
 	remaining_enemies += 1
 
-func spawn_all_enemies(room):
+func spawn_all_enemies(room, num_enemies):
 	var spawn_points = room.get_node("EnemySpawnPoint")
 	if spawn_points == null:
-		print("Warn: no spawn points available")
-	else:
-		print("Debug: spawning", len(spawn_points.get_children()), " enemies")
-		for point in spawn_points.get_children():
-			self.spawn_enemy("random",point.global_position)
+		#print("Warn: no spawn points available")
+		return
+	
+	#print("Debug: spawning ", len(spawn_points.get_children()), " enemies")
+	var points = spawn_points.get_children()
+	for index in range(num_enemies):
+		self.spawn_enemy("random",points[index%len(points)].global_position)
 			
 func remove_all_enemies():
 	for enemy in get_tree().get_nodes_in_group("enemy"):
@@ -94,7 +99,6 @@ func on_enemy_died(at_position):
 		open_exit_door()
 
 func open_exit_door():
-	print("Unlocking door")
 	var doorblocker = get_tree().get_first_node_in_group("door_blocker")
 	if doorblocker != null:
 		doorblocker.queue_free()
@@ -110,13 +114,14 @@ func generate_new_room(room):
 	elif room_counter >= randi_range(2,4):
 		new_room_cls = load(ROOM_TRADER)
 		room_counter = 0
+		times_trader_was_visited += 1
 	else:
 		new_room_cls = load(ROOM_NORMAL.pick_random())
 		room_counter += 1
 		
 	if DEBUG and TO_TRADER:
 		new_room_cls = load(ROOM_TRADER)
-	print("Debug: Spawning new room")
+	print("Debug: Spawning new room ", room)
 	return new_room_cls.instantiate()
 	
 func enter_new_room(room="normal"):
@@ -125,10 +130,16 @@ func enter_new_room(room="normal"):
 	self.call_deferred("add_child",new_room)
 	current_room = new_room
 	
+	var num_enemies = 0
+	if room == "normal":
+		num_enemies = NUM_START_ENEMIES + times_trader_was_visited 
+	elif room == "tutorial":
+		num_enemies = 1
+		times_trader_was_visited = 0
 	var new_pos = new_room.get_node("EnterPoint").global_position
 	PLAYER_CHARACTER.set_global_position(new_pos)
 	remove_all_enemies()
-	spawn_all_enemies(new_room)
+	spawn_all_enemies(new_room, num_enemies)
 	remove_all_coins()
 	if remaining_enemies <= 0:
 		open_exit_door()
